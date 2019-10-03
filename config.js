6b6f93e53ef8f20ed1e7f1c38e11f11c86612b28
Роман <RSimashev@croc.ru>
2019-10-03 17:19:15 +0300

    second commit

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBTgezGAUPxYfENNQRsunGxyvG8LeBo-Ls",
+    authDomain: "react-consult.firebaseapp.com",
+    databaseURL: "https://react-consult.firebaseio.com",
+    projectId: "react-consult",
+    storageBucket: "",
+    messagingSenderId: "860818084999",
+    appId: "1:860818084999:web:004d8ff050004e76f61422",
+    measurementId: "G-4J125QLYKF"
+};
